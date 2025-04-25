dd851c00981e363097855a138dfaabda12887f55
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2025-04-24 21:27:14 -0300

    Adição do nivel 3 testes

--- src/component/MazeMap.jsx
+++ src/component/MazeMap.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react';
 import matrizes1 from '../../public/matrizesLvL1.json';
-//import matrizes2 from '../../public/matrizesLvL2.json';
-import matrizes2 from '../../public/teste.json';
+import matrizes2 from '../../public/matrizesLvL2.json';
+import matrizes3 from '../../public/teste.json';
 
 export default function useMaze(nivel) {
   let matrizes
   if (nivel===1){
     matrizes = matrizes1
-  }else {
+  }else if(nivel===2){
     matrizes = matrizes2
   }
+  else if (nivel===3){
+    matrizes = matrizes3
+  }
   const [mazeLayout, setMazeLayout] = useState(() => {
-    const randomIndex = Math.floor(Math.random() * matrizes2.length);
+    const randomIndex = Math.floor(Math.random() * matrizes.length);
     return matrizes[randomIndex];
   });
 
   const gerarNovoMaze = () => {
-    const randomIndex = Math.floor(Math.random() * matrizes2.length);
+    const randomIndex = Math.floor(Math.random() * matrizes.length);
     setMazeLayout(matrizes[randomIndex]);
   };
 
