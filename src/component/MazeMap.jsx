f3555ad9243d2c2a71984fe58647d28abd52949e
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2025-05-07 20:20:44 -0300

    MazeMap updates

--- src/component/MazeMap.jsx
+++ src/component/MazeMap.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import mapaMobileLv1 from "../../public/mapaMobileNv1.json"  // 16 x 32
 import mapaMobileLv2 from "../../public/mapaMobileNv2.json"  // 20 x 40
 import mapaMobileLv3 from "../../public/mapaMobileNv3.json"  // 24 x 48
-import matrizes1 from '../../public/matrizesLvL1.json';
-import matrizes2 from '../../public/matrizesLvL2.json';
-import matrizes3 from '../../public/teste.json';
+import matrizes1 from '../../public/matrizesLvLTeste1.json';  // 30 x 15
+import matrizes2 from '../../public/matrizesLvLTeste3.json';  // 40 x 20
+import matrizes3 from '../../public/matrizesLvLTeste3.json';  // 50 x 25
 
 export default function useMaze(nivel) {
   const escolherMatrizes = () => {
