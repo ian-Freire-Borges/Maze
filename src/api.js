2598d2c075fb42daec77ea59e9eaf4740787d1d1
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2025-05-12 23:35:12 -0300

    Adicionando middlewares

--- src/api.js
+++ src/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: 'https://mazeapi.onrender.com',
 });
 
+api.interceptors.request.use((config) => {
+  const token = import.meta.env.VITE_API_TOKEN;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
