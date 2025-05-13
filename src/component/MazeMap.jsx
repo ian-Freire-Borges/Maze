1765aa4afce85a530b82f99dad1e38ac5f402d0a
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2025-05-12 22:45:27 -0300

    Adicionando matriz teste

--- src/component/MazeMap.jsx
+++ src/component/MazeMap.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import mapaMobileLv1 from "../../public/Mobile1.json"  // 16 x 32
 import mapaMobileLv2 from "../../public/Mobile2.json"  // 22 x 44
 import mapaMobileLv3 from "../../public/Mobile3.json"  // 24 x 48
-import matrizes1 from '../../public/mapaNv1.json' // 30 x 15
-import matrizes2 from '../../public/mapaNv2.json';  // 40 x 20
-import matrizes3 from '../../public/mapaNv3.json';  // 50 x 25
-import demo from '../../public/mapaForDemo.json';
+import matrizes1 from '../../public/mapaNv1.json' // 35 x 19
+import matrizes2 from '../../public/mapaNv2.json';  // 45 x 25
+import matrizes3 from '../../public/mapaNv3.json';  // 55 x 29
+import demo from '../../public/mapaForDemo.json';   // 35 x 19
 
 export default function useMaze(nivel) {
   const escolherMatrizes = () => {
