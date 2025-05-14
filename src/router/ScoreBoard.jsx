0d3fc37e4e1a8df4040a4afaee1b505aba3111df
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2025-05-14 19:12:43 -0300

    Adicao do banco de dados pro scorenivel

--- src/router/ScoreBoard.jsx
+++ src/router/ScoreBoard.jsx
@@ -4,35 +4,50 @@ import api from '../api';
 
 const ScoreBoard = ({ setScreen }) => {
   const [scores, setScores] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [viewNivel, setViewNivel] = useState(false);
 
   useEffect(() => {
-    async function fetchScores() {
+    const fetchData = async () => {
+      setLoading(true);
       try {
-        const response = await api.get('/scoreTop');
+        const response = await api.get(viewNivel ? '/scoreNivel' : '/scoreTop');
         setScores(response.data);
       } catch (error) {
         console.error('Erro ao buscar scores:', error);
+        setScores([]);
+      } finally {
+        setLoading(false);
       }
-    }
+    };
 
-    fetchScores();
-  }, []);
+    fetchData();
+  }, [viewNivel]);
 
   return (
     <div className="scoreboard-wrapper">
       <div className="scoreboard-container">
-        <h2>Top 10 Scores</h2>
-        <ul className="score-list">
-          {scores.map((score) => (
-            <li key={score.id}>
-              <span className='score-info'>
-                <strong>{score.name}</strong> - {score.score} pts
-              </span>
-            </li>
-          ))}
-        </ul>
-        <div className='back-button'>
-          <button onClick={() => setScreen("MENU")}>Voltar</button>
+        <h2>{viewNivel ? 'Top 10 Modo Infinito' : 'Top 10 Scores'}</h2>
+
+        {loading ? (
+          <p>Carregando...</p>
+        ) : (
+          <ul className="score-list">
+            {scores.map((score) => (
+              <li key={score.id}>
+                <span className="score-info">
+                  <strong>{score.name}</strong> - {viewNivel ? `${score.nivel}° nivel` : `${score.score} pts`}
+                </span>
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <div className="back-button">
+          <button onClick={() => setScreen('MENU')}>Voltar</button>
+          <button onClick={() => setViewNivel(!viewNivel)}>
+            {viewNivel ? 'Score Normal' : 'Score Infinito'}
+          </button>
         </div>
       </div>
     </div>
