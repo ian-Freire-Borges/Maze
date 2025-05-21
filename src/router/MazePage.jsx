1459c38d74cabaa7f73f5f9ddc2b4a823bbb9624
Ian Freire Borges <ianxofreire@gmail.com>
2025-05-21 00:53:10 -0300

    teste para mobile

--- src/router/MazePage.jsx
+++ src/router/MazePage.jsx
@@ -53,8 +53,8 @@ useEffect(() => {
       let maxHeight;
       let maxWidth ;
       if(window.innerWidth <= 450 ){
-          maxHeight = window.innerHeight * 0.9;
-          maxWidth = window.innerWidth * 0.9;
+          maxHeight = window.innerHeight * 1;
+          maxWidth = window.innerWidth * 1;
       }else{
       maxWidth = window.innerWidth * 1;
       maxHeight = window.innerHeight * 0.8;
